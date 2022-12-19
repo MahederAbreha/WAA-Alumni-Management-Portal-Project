e0aecd0d7dfbf9c7a317725ad454109f4ae80869
eudism <115727438+eudism@users.noreply.github.com>
2022-12-18 21:04:20 -0600

    Added Address and JobExprience Entities

---
+++ api/src/main/java/edu/miu/project/entity/JobExperience.java
@@ -0,0 +1,14 @@
+package edu.miu.project.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class JobExperience {
+   @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Year;
+   private  String countryName;
+}
